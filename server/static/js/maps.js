9554c5a44d502ea07b7362ea8e11984cfa6b8448
Nikola Radnovic <meda@berkeley.edu>
2014-03-09 17:24:03 -0700

    Fixing console JS errors

--- server/static/js/maps.js
+++ server/static/js/maps.js
@@ -37,52 +37,54 @@ function cal_map(address, id) {
 			mapTypeId: 'roadmap'
 	}
 
-  	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+	if (document.getElementById('map-canvas') != null) {
+  		map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-	// Create the search box and link it to the UI element.
-	var input = document.getElementById('newslot_location');
-	var searchBox = new google.maps.places.SearchBox(input);
+		// Create the search box and link it to the UI element.
+		var input = document.getElementById('newslot_location');
+		var searchBox = new google.maps.places.SearchBox(input);
 	
-	// Listen for the event fired when the user selects an item from the
-	// pick list. Retrieve the matching places for that item.
-	google.maps.event.addListener(searchBox, 'places_changed', function() {
-		var places = searchBox.getPlaces();
+		// Listen for the event fired when the user selects an item from the
+		// pick list. Retrieve the matching places for that item.
+		google.maps.event.addListener(searchBox, 'places_changed', function() {
+			var places = searchBox.getPlaces();
 
-		for (var i = 0, marker; marker = markers[i]; i++) {
-			marker.setMap(null);
-		}
+			for (var i = 0, marker; marker = markers[i]; i++) {
+				marker.setMap(null);
+			}	
 
-		// For each place, get the icon, place name, and location.
-		markers = [];
-		var bounds = new google.maps.LatLngBounds();
-		for (var i = 0, place; place = places[i]; i++) {
-			var image = {
-				url: place.icon,
-				size: new google.maps.Size(71, 71),
-				origin: new google.maps.Point(0, 0),
-				anchor: new google.maps.Point(17, 34),
-				scaledSize: new google.maps.Size(25, 25)
-			};
+			// For each place, get the icon, place name, and location.
+			markers = [];
+			var bounds = new google.maps.LatLngBounds();
+			for (var i = 0, place; place = places[i]; i++) {
+				var image = {
+					url: place.icon,
+					size: new google.maps.Size(71, 71),
+					origin: new google.maps.Point(0, 0),
+					anchor: new google.maps.Point(17, 34),
+					scaledSize: new google.maps.Size(25, 25)
+				};
 
-			 // Create a marker for each place.
-			 var marker = new google.maps.Marker({
-			   map: map,
-			   icon: image,
-			   title: place.name,
-			   position: place.geometry.location
-			 });
+			 	// Create a marker for each place.
+			 	var marker = new google.maps.Marker({
+			  	  map: map,
+			      icon: image,
+			      title: place.name,
+			      position: place.geometry.location
+			    });
 
-			markers.push(marker);
-			bounds.extend(place.geometry.location);
-		}
-		map.fitBounds(bounds);
-		map.setZoom(map.getZoom()-6);
-	});
+				markers.push(marker);
+				bounds.extend(place.geometry.location);
+			}
+			map.fitBounds(bounds);
+			map.setZoom(map.getZoom()-6);
+		});
 
-// Bias the SearchBox results towards places that are within the bounds of the
-// current map's viewport.
-	google.maps.event.addListener(map, 'bounds_changed', function() {
-		var bounds = map.getBounds();
-		searchBox.setBounds(bounds);
-	});
+	// Bias the SearchBox results towards places that are within the bounds of the
+	// current map's viewport.
+		google.maps.event.addListener(map, 'bounds_changed', function() {
+			var bounds = map.getBounds();
+			searchBox.setBounds(bounds);
+		});
+	};
 };
