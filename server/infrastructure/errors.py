b4338b258728321b5902101dc21b882b102af519
Corey Hyllested <corey@herotime.co>
2014-03-12 11:58:18 -0700

    add NoProposalFound error

--- server/infrastructure/errors.py
+++ server/infrastructure/errors.py
@@ -1,9 +1,18 @@
 import sys
+from datetime import datetime as dt
 
 
 #IndexError: list index out of range
 #seen: we tried to index a list (from DB) that was zero
 
+
+class NoProposalFound(Exception):
+	def __init__(self, p_uuid, p_from):
+		self.prop_uuid = p_uuid
+		self.prop_from = p_from
+		
+	def __str__(self):
+		return "Proposal (%s, %s) not found" % (self.prop_uuid, self.prop_from)
 
 
 class InvalidCreditCard(Exception):
