3d57ae6a007f80ef84edf52641ea21273bce92c9
Corey Hyllested <corey@herotime.co>
2014-05-08 22:15:39 -0700

    new aws db

--- server/infrastructure/srvc_database.py
+++ server/infrastructure/srvc_database.py
@@ -26,8 +26,8 @@ from sqlalchemy.ext.declarative import declarative_base
 #    db_session.remove()
 
 print 'import::db -- create engine'
-engine = create_engine('postgresql://brjwzdmhzipgwg:nYIJrHWrnkRruzOdzzCdFqKdXp@ec2-54-204-43-200.compute-1.amazonaws.com:5432/d23r20cnqg2c72')
-#engine = create_engine('postgresql://htdb:passw0rd@htdb.cesf5wqzwzr9.us-east-1.rds.amazonaws.com:5432/htdb')
+#engine = create_engine('postgresql://brjwzdmhzipgwg:nYIJrHWrnkRruzOdzzCdFqKdXp@ec2-54-204-43-200.compute-1.amazonaws.com:5432/d23r20cnqg2c72')
+engine = create_engine('postgresql://htdb:passw0rd@htdb.cesf5wqzwzr9.us-east-1.rds.amazonaws.com:5432/htdb')
 
 print 'import::db -- create sessionmaker'
 db_session = scoped_session(sessionmaker(bind=engine))
