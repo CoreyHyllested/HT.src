b8d4f4dcc117811e2939cb10ea9972aec845cc84
flux3000 <ryanfbaker@gmail.com>
2014-06-17 16:55:05 -0700

    fixed bug when msg recip is not set

--- server/views.py
+++ server/views.py
@@ -1384,7 +1384,12 @@ def render_inbox_page():
 def render_compose_page():
 	hid = request.values.get('hp')
 	bp = Profile.get_by_uid(session['uid'])
-	hp = Profile.get_by_prof_id(hid)
+
+	if (hid is not None):
+		hp = Profile.get_by_prof_id(hid)
+	else:
+		hp = None
+
 	return make_response(render_template('compose.html', bp=bp, hp=hp))
 
 
