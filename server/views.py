292c348a253f4e80138307934da764577153477f
Corey Hyllested <corey@herotime.co>
2014-07-04 18:38:23 -0600

    can see images in /email_template

--- server/views.py
+++ server/views.py
@@ -25,6 +25,11 @@ from werkzeug.security import generate_password_hash #rm -- should be in control
 
 
 
+@ht_server.route('/email_template', methods=['GET', 'POST'])
+def render_email_template():
+	return make_response(render_template('generic_email_template.html'))
+
+
 @ht_server.route('/', methods=['GET', 'POST'])
 @ht_server.route('/index')
 @dbg_enterexit
