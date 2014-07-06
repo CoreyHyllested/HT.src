1cfc54dd39ba077969b168c7aaafd950cc5a9e3d
Corey Hyllested <corey@herotime.co>
2014-07-05 19:34:38 -0600

    need a default action, and want to run with SSL, not SSLify

--- application.py
+++ application.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
-
-from server import application
+import os
+from server import  initialize_server
+from flask.ext.script import Manager, Shell
 from OpenSSL import SSL
 
+application = initialize_server(os.getenv('INSPRITE_CONFIG') or 'default')
+manager = Manager(application)
+
+def mk_insprite_shell_ctx():
+	return dict(app=application)
+
+manager.add_command("shell", Shell(make_context=mk_insprite_shell_ctx))
 
 context = SSL.Context(SSL.SSLv23_METHOD)
 context.use_privatekey_file('security/herotime-pk.pem')
@@ -14,4 +22,4 @@ def main():
 
 
 if __name__ == "__main__":
-	main()
+	manager.run()
