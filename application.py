ce96477e19c86ae9e922602a766a21c50ed77fd3
Corey Hyllested <corey@herotime.co>
2014-07-06 11:20:48 -0600

    if no options are added run ssh, as normal.  If more than one is added.  Run shell or runserver

--- application.py
+++ application.py
@@ -1,25 +1,35 @@
 #!/usr/bin/env python
-import os
+import os, sys
 from server import  initialize_server
 from flask.ext.script import Manager, Shell
 from OpenSSL import SSL
 
+
 application = initialize_server(os.getenv('INSPRITE_CONFIG') or 'default')
-manager = Manager(application)
 
-def mk_insprite_shell_ctx():
-	return dict(app=application)
 
-manager.add_command("shell", Shell(make_context=mk_insprite_shell_ctx))
 
-context = SSL.Context(SSL.SSLv23_METHOD)
-context.use_privatekey_file('security/herotime-pk.pem')
-context.use_certificate_file('security/herotime.crt')
+def run_manager():
+	def mk_insprite_shell_ctx():
+		return dict(app=application)
+
+	manager = Manager(application)
+	manager.add_command("shell", Shell(make_context=mk_insprite_shell_ctx))
+	manager.run()
 
 
 def main():
+	print 'Run with SSH'
+	context = SSL.Context(SSL.SSLv23_METHOD)
+	context.use_privatekey_file('security/herotime-pk.pem')
+	context.use_certificate_file('security/herotime.crt')
 	application.run(debug = True, ssl_context=context)
 
 
+
+
 if __name__ == "__main__":
-	manager.run()
+	if (len(sys.argv) >= 2):
+		run_manager()
+	else:
+		main()
