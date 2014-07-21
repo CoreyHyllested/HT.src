0bcbc56baef3b5780aea769635ea520d08cf49c8
Corey Hyllested <corey@herotime.co>
2014-07-21 10:40:54 -0600

    header

--- server/infrastructure/tasks.py
+++ server/infrastructure/tasks.py
@@ -1,3 +1,17 @@
+#################################################################################
+# Copyright (C) 2014 Insprite, LLC.
+# All Rights Reserved.
+#
+# All information contained is the property of Insprite, LLC.  Any intellectual
+# property about the design, implementation, processes, and interactions with
+# services may be protected by U.S. and Foreign Patents.  All intellectual
+# property contained within is covered by trade secret and copyright law.
+#
+# Dissemination or reproduction is strictly forbidden unless prior written
+# consent has been obtained from Insprite, LLC.
+#################################################################################
+
+
 from __future__ import absolute_import
 from datetime import datetime as dt, timedelta
 from server import ht_server
