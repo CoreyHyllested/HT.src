40b768515a6e40a01ff3e2f6dba57316994643a6
Corey Hyllested <corey@herotime.co>
2014-07-25 11:35:45 -0600

    cleanup Email, Image models

--- server/models/__init__.py
+++ server/models/__init__.py
@@ -1,7 +1,21 @@
+#################################################################################
+# Copyright (C) 2014 Insprite, LLC.
+# All Rights Reserved.
+#
+# All information contained is the property of Insprite, LLC.  Any intellectual
+# property about the design, implementation, processes, and interactions with
+# services may be protected by U.S. and Foreign Patents.  All intellectual
+# property contained within is covered by trade secret and copyright law.
+#
+# Dissemination or reproduction is strictly forbidden unless prior written
+# consent has been obtained from Insprite, LLC.
+#################################################################################
+
+
 from .Account import Account
 from .Profile import Profile
 from .Email import *
-from .Image import Image
+from .Image import *
 from .Industry import Industry
 from .LessonImageMap import LessonImageMap
 from .Lesson import Lesson
