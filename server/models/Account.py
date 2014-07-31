178aa6426ad690d67b52a7e1dd27cead9870c6f1
Corey Hyllested <corey@herotime.co>
2014-07-31 12:44:03 -0600

    Account.email_policy (default  = 0)

--- server/models/Account.py
+++ server/models/Account.py
@@ -55,6 +55,7 @@ class Account(Base):
 	sec_answer   = Column(String(128))
 	stripe_cust	 = Column(String(64))
 	role		 = Column(Integer, default = 0)
+	email_policy = Column(Integer, default = 0)
 
 	# all user profiles
 	profiles = relationship('Profile', cascade='all,delete', uselist=False, lazy=False)
