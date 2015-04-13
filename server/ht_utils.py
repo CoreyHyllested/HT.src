5ab3e61c224887d260c536a071804c3845de7458
Corey Hyllested <corey@herotime.co>
2015-04-13 12:21:19 -0600

    added @deprecated

--- server/ht_utils.py
+++ server/ht_utils.py
@@ -1,42 +1,66 @@
 #################################################################################
-# Copyright (C) 2013 - 2014 Insprite, LLC.
+# Copyright (C) 2015 Soulcrafting
 # All Rights Reserved.
 #
-# All information contained is the property of Insprite, LLC.  Any intellectual
+# All information contained is the property of Soulcrafting. Any intellectual
 # property about the design, implementation, processes, and interactions with
-# services may be protected by U.S. and Foreign Patents.  All intellectual
+# services may be protected by U.S. and Foreign Patents. All intellectual
 # property contained within is covered by trade secret and copyright law.
 #
 # Dissemination or reproduction is strictly forbidden unless prior written
-# consent has been obtained from Insprite, LLC.
+# consent has been obtained from Soulcrafting.
 #################################################################################
 
 
-import logging
-import functools
-from server import ht_server
+import logging, functools
+from server import sc_server
 from flask import *
 
 
 def trace(msg):
-	ht_server.logger.info(msg)
+	sc_server.logger.info(msg)
+
+def sc_debug(msg):
+	if sc_server.debug: print(msg)
 
 def ht_debug(msg):
-	#if ht_server.debug: ht_server.logger.info(msg)
-	if ht_server.debug: print(msg)
+	#if sc_server.debug: sc_server.logger.info(msg)
+	sc_debug(msg)
 
 def log_uevent(uid, msg):
-	ht_server.logger.info(str(uid) + " " + str(msg))
-	
+	sc_server.logger.info(str(uid) + " " + str(msg))
+
+
+################################################################################
+### ANNOTATIONS ################################################################
+################################################################################
+
+def authentication(function):
+	@functools.wraps(function)
+	def verify_authenticated_user(*args, **kwargs):
+		if 'uid' not in session:
+			trace("no uid; " + function.__name__ + ': redirect to login')
+			return make_response(redirect('/login'))
+		return function(*args, **kwargs)
+	return verify_authenticated_user
+
+
+def deprecated(function):
+	@functools.wraps(function)
+	def print_deprecated(*args, **kwargs):
+		print '**DEPRECATED** ' + str(function.__name__)
+		return function(*args, **kwargs)
+	return print_deprecated
+
 
 
 def dbg_enterexit(orig_fn):
 	@functools.wraps(orig_fn)
 	def logged_fn(*args, **kwargs):
 		uid = session.get('uid')
-		ht_debug(orig_fn.__name__ + ': enter ' + str(uid))
+		sc_debug(orig_fn.__name__ + ': enter ' + str(uid))
 		rc = orig_fn(*args, **kwargs)
-		ht_debug(orig_fn.__name__ + ': rc = ' + str(rc))
+		sc_debug(orig_fn.__name__ + ': rc = ' + str(rc))
 		return rc
 	return logged_fn
 
