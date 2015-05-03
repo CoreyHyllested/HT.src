0f9329164fa80d4da54cc6ce439c12268b66c694
Corey Hyllested <corey@herotime.co>
2015-05-03 15:26:32 -0600

    init skrollr

--- server/static/js/about.js
+++ server/static/js/about.js
@@ -1,4 +1,14 @@
 $(document).ready(function () {
 	console.log('about.js: ready');
 	$('#masthead-text').css('opacity', '1');
+
+
+	// Init Skrollr
+	var skrl = skrollr.init({
+		render:	function(data) {
+			//Debugging - Log the current scroll position.
+			console.log(data.curTop);
+				}
+	});
+
 });
