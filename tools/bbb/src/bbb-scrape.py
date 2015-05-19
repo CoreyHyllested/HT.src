cc2a8644d957ff55d48cf44e9752c7c267357fbf
Corey Hyllested <corey@herotime.co>
2015-05-19 10:25:50 -0600

    cleaning up

--- tools/bbb/src/bbb-scrape.py
+++ tools/bbb/src/bbb-scrape.py
@@ -5,8 +5,15 @@ from bs4 import BeautifulSoup, Comment
 from bs4 import BeautifulSoup as Soup
 from soupselect import select
 import feedparser
-import re
+import re, os
 
+
+
+def open_file(name):
+	filename = os.getcwd() + '/data/preprocessed/reviews/' + name
+	print 'creating file ' + str(filename)
+	fp = open(filename, 'a+')
+	return fp
 
 
 def get_bbb_types(page):
@@ -167,11 +174,9 @@ def get_businesses_by_type(page):
 	dom	= urllib2.urlopen(page).read()
 	dom_soup = BeautifulSoup(dom)
 
-	#local_businesses = dom_soup.find_all(id=re.compile('list[0-9]+'))
 	local_businesses = dom_soup.find_all(itemtype='http://schema.org/LocalBusiness')
 	print len(local_businesses), 'businesses found'
 	for business in local_businesses:
-		#sponsor = business.find_all(class_=['sponsorBx'])
 		addr	= business.find_all(itemtype='http://schema.org/PostalAddress')[0]
 		name	= business.find_all(itemprop='name')[0].get_text()
 		phone	= business.find_all(itemprop='phone')[0].get_text()
@@ -180,71 +185,80 @@ def get_businesses_by_type(page):
 
 		bbb_uri	= business.find_all(itemprop='name')[0].attrs.get('href')
 
+		img = None
 		if len(image) > 0:
-			image	= image[0].attrs.get('src')
+			img	= image[0].attrs.get('src')
 			#print 'found logo', image
-		else:
-			image = None
-
 
 		for uri in links:
 			URI = uri.attrs.get('href')
-			print URI
+			#print URI
 			if (('maps.google.com' not in URI) and ('www.bbb.org' not in URI)):
 				link = URI
 
 
-		bbb_parse_address(name, addr, phone, link, image, bbb_uri)
+		bbb_parse_address(name, addr, phone, link, img, bbb_uri)
 		bbb_parse_business(bbb_uri)
-		bbb_parse_business_reviews(bbb_uri)
+		bbb_parse_business_reviews(name, bbb_uri)
 
 
-def bbb_parse_business_reviews(page):
+def bbb_parse_business_reviews(name, page):
 	print 'About to scrape "BBB Business Page": ' + str(page)
 	page = page + '/customer-reviews'
 	reviews_dom	= urllib2.urlopen(page).read()
 	reviews_soup = BeautifulSoup(reviews_dom)
-	positive = reviews_soup.find(id='cr-pos-listing')
-	reviews	= positive.find_all('tr')
-	for r in reviews:
-		r_date		= r.find(class_=['td_h']).get_text()
-		r_details	= r.find(class_=['td_detail']).get_text()
-		comments	= r.findAll(text=lambda text:isinstance(text, Comment))	#has email address, use the get project price by pulling permits?
-		print r_date
-		details = r_details[0:r_details.find('This customer had a')]
-		print details
 
-	negative = reviews_soup.find(id='cr-neg-listing')
-	reviews	= negative.find_all('tr')
-	for r in reviews:
-		r_date		= r.find(class_=['td_h']).get_text()
-		r_details	= r.find(class_=['td_detail']).get_text()
-		#comments	= r.findAll(text=lambda text:isinstance(text, Comment))	#has email address, use the get project price by pulling permits?
-		print r_date
-		details = r_details[0:r_details.find('This customer had a')]
-		print details
-		
-	neutral	 = reviews_soup.find(id='cr-neu-listing')
-	reviews	= neutral.find_all('tr')
-	for r in reviews:
-		r_date		= r.find(class_=['td_h']).get_text()
-		r_details	= r.find(class_=['td_detail']).get_text()
-		#comments	= r.findAll(text=lambda text:isinstance(text, Comment))	#has email address, use the get project price by pulling permits?
-		print r_date
-		details = r_details[0:r_details.find('This customer had a')]
-		print details
+	pos_reviews	= reviews_soup.find(id='cr-pos-listing')
+	neg_reviews	= reviews_soup.find(id='cr-neg-listing')
+	neu_reviews	= reviews_soup.find(id='cr-neu-listing')
+
+	positive	= pos_reviews.find_all('tr')
+	negative	= neg_reviews.find_all('tr')
+	neutral		= neu_reviews.find_all('tr')
+	nr_reviews	= len(positive) + len(negative) + len(neutral)
+	print nr_reviews
+
+	fp = None
+	if (nr_reviews):
+		# pass in business name to bbb_parse_bus_page & replace 'weirdchars'
+		fp = open_file(name)
 	
+	for review in positive:
+		r_date		= review.find(class_=['td_h']).get_text()
+		r_details	= review.find(class_=['td_detail']).get_text()
+		details		= r_details[0:r_details.find('This customer had a')]
+		comments	= review.findAll(text=lambda text:isinstance(text, Comment))	#has email address, use the get project price by pulling permits?
+		if (fp is not None): fp.write(details)
+		#print details
+
+	for review in negative:
+		r_date		= review.find(class_=['td_h']).get_text()
+		r_details	= review.find(class_=['td_detail']).get_text()
+		details		= r_details[0:r_details.find('This customer had a')]
+		#comments	= review.findAll(text=lambda text:isinstance(text, Comment))	#has email address, use the get project price by pulling permits?
+		print details
+		if (fp is not None): fp.write(details)
+		
+	for review in neutral:
+		r_date		= review.find(class_=['td_h']).get_text()
+		r_details	= review.find(class_=['td_detail']).get_text()
+		details = r_details[0:r_details.find('This customer had a')]
+		#comments	= review.findAll(text=lambda text:isinstance(text, Comment))	#has email address, use the get project price by pulling permits?
+		print details
+		if (fp is not None): fp.write(details)
+	
+	if (fp is not None): fp.close()
 
 
 def bbb_parse_business(page):
-	print 'About to scrape "BBB Business Page": ' + str(page)
+	print 'scraping "BBB Business Page"  ' + str(page)
 	dom	= urllib2.urlopen(page).read()
 	dom_soup = BeautifulSoup(dom)
 
-	print 'Finding LocalBusiness'
+	#print 'Finding LocalBusiness'
 	business = dom_soup.find_all(itemtype='http://schema.org/LocalBusiness')[0]
 
-	print 'Get name, telephone, address'
+	#print 'Get name, telephone, address'
 	bus_name	= business.find_all(itemprop='name')[0].get_text()
 	bus_phone	= business.find_all(itemprop='telephone')[0].get_text()
 	bus_address	= business.find_all(itemtype='http://schema.org/PostalAddress')[0]
@@ -256,14 +270,13 @@ def bbb_parse_business(page):
 
 #	bus_contact_url	= business.find_all(class_='business-link')[0].get_text()
 
-	print 'Get accredited information'
+	#print 'Get accredited information'
 	accredited_since	= business.find_all(class_='accredited-since')[0].get_text()
 	accredited_rating	= business.find_all(id='accedited-rating')	#BBB misspells this, check to see if it got fixed
-#	print accredited_since, accredited_rating
 	rating = accredited_rating[0].img.attrs.get('title')
 	print accredited_since, rating
 	
-	print 'Get additional business information'
+	#print 'Get additional business information'
 	bus_additional = business.find(id='business-additional-info-container')
 	bus_bbb_open = bus_additional.find('span').get_text()
 	bus_founding	= bus_additional.find(itemprop='foundingDate').get_text()	#also just business.search
@@ -272,27 +285,27 @@ def bbb_parse_business(page):
 	if bus_info_legal is not None:
 		bus_info_legal = bus_info_legal.nextSibling.get_text()
 
-	print bus_bbb_open
-	print bus_founding
-	print bus_info_legal
+	print 'Business Opened:  ' + str(bus_bbb_open)
+	print 'Business Founded: ' + str(bus_founding)
+	print 'Business Legal Status: ' + str(bus_info_legal)
 
-	print 'Get employee information'
+	#print 'Get employee information'
 	bus_info_employees = bus_additional.find(itemprop='employees')
 	if bus_info_employees == None:
 		bus_emp = bus_additional.find('h5', text='Business Management').nextSibling.get_text()
 		print bus_emp
 		bus_info_employees = []
 	for emp in bus_info_employees:
-		print 'Job title?: ', emp.get_text()
+		#print 'Job title?: ', emp.get_text()
 		emp_name = emp.find(itemprop='name')
 		if (emp_name is not None): emp_name = emp_name.get_text()
 		emp_title = emp.find(itemprop='jobTitle')
 		if (emp_title is not None): emp_title = emp_title.get_text()
-		print 'person name:', emp_name
-		print 'person job:', emp_title
+		#print 'person name:', emp_name
+		#print 'person job:', emp_title
 
 
-	print 'Get category information'
+	#print 'Get category information'
 	bus_info_categories	= bus_additional.find('h5', text='Business Category').nextSibling.get_text().split(',')
 	print bus_info_categories
 	
@@ -324,9 +337,11 @@ if __name__ == '__main__':
 	types = get_bbb_types_cached()
 	for business_type in types:
 		get_businesses_by_type(business_type)
-#	bbb_parse_business('http://www.bbb.org/denver/business-reviews/roofing-contractors/303-933-roof-in-littleton-co-75003621')
-#	bbb_parse_business_reviews('http://www.bbb.org/denver/business-reviews/roofing-contractors/303-933-roof-in-littleton-co-75003621')
+
 		
 
 
 
+#EXAMPLE dom_soup.find_all(id=re.compile('list[0-9]+'))
+#	testbbb_parse_business('http://www.bbb.org/denver/business-reviews/roofing-contractors/303-933-roof-in-littleton-co-75003621')
+#	testbbb_parse_business_reviews('http://www.bbb.org/denver/business-reviews/roofing-contractors/303-933-roof-in-littleton-co-75003621')
