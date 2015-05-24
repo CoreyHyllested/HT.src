21081183836dfc7ade4add58990b9a872160fbfb
Corey Hyllested <corey@herotime.co>
2015-05-24 14:11:19 -0600

    set expiration on images

--- server/routes/meta.py
+++ server/routes/meta.py
@@ -33,15 +33,18 @@ def meta_serve_from_root():
 #  FOR MORE INFO.
 #	http://flask.pocoo.org/docs/0.10/api/#flask.after_this_request
 #	http://flask.pocoo.org/docs/0.10/api/#flask.send_file (kwargs)
-#
-#@sc_meta.route('/static/img/<image>')
-#def meta_serve_static_img(image):
-#	server_root_dir = sc_server.static_folder + '/img/'
-#	@after_this_request
-#	def add_header(response):
-#		response.headers['Cache-Control'] = '84600'
-#		return response
-#	return send_from_directory(server_root_dir, str(image))
+#	Not using x-sendfile (we need the HTTPD module mod_xsendfile)...
+#   http://makandracards.com/makandra/990-speed-up-file-downloads-with-rails-apache-and-x-sendfile
+#	Other potentioal solution: https://github.com/corydolphin/flask-headers
+@sc_meta.route('/static/img/<image>')
+def meta_serve_static_img(image):
+	@after_this_request
+	def add_header(response):
+		#print dir(response.cache_control)
+		response.cache_control.max_age=5184000
+		return response
+	server_root_dir = sc_server.static_folder + '/img/'
+	return send_from_directory(server_root_dir, str(image))
 
 
 
