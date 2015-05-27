56c7d4f9df6a5dda85f5165adfef91da6d2b7030
Corey Hyllested <corey@herotime.co>
2015-05-27 15:33:14 -0600

    this is going to suck, webcache is rate-limiting us too.

--- tools/framework/sources/BBB.py
+++ tools/framework/sources/BBB.py
@@ -11,9 +11,89 @@
 # consent has been obtained from Soulcrafting.
 #################################################################################
 
-from Source import Source
+from Source import *
+from pprint import pprint as pp
+from bs4 import BeautifulSoup, Comment
+
+
 
 class BBB(Source):
+	SOURCE_DIR	= 'bbb/'
+	USE_WEBCACHE = True
+
 	def __init__(self):
 		super(BBB, self).__init__()
+		self.errors = {}
 		print '\tcreating BBB obj'
+
+
+	def bbb_top_directories(self):
+		rel_path = '/data/sources/' + self.SOURCE_DIR + '/directories.json'
+		json_data	= self.read_json_file(rel_path)
+		directories	= json_data.get('directories', [])
+		return directories
+
+
+	def bbb_top_directories_testing(self):
+		return  [ "http://www.bbb.org/denver/accredited-business-directory/deck-builder" ]
+
+
+	def bbb_scrape_directory_page(self, page):
+		if (self.USE_WEBCACHE):
+			page = webcache_url(page)
+
+		print 'checkout out :', page
+
+		try:
+			document = urllib2.urlopen(page).read()
+		except urllib2.HTTPError as e:
+			print e
+			self.errors[e.geturl()] = e.code
+			return
+		except Exception as e:
+			print type(e), e
+			return
+
+		dom_soup = BeautifulSoup(document)
+		local_businesses = dom_soup.find_all(itemtype='http://schema.org/LocalBusiness')
+		print len(local_businesses), 'businesses found'
+		for business in local_businesses:
+			addr	= business.find_all(itemtype='http://schema.org/PostalAddress')[0]
+			name	= business.find_all(itemprop='name')[0].get_text()
+			phone	= business.find_all(itemprop='phone')[0].get_text()
+			image	= business.find_all(itemtype='http://schema.org/ImageObject')
+			links	= business.find_all(class_=['link', 'newtab'])
+
+			bbb_uri	= business.find_all(itemprop='name')[0].attrs.get('href')
+
+			img = None
+			if len(image) > 0:
+				img	= image[0].attrs.get('src')
+				#print 'found logo', image
+
+			link = None
+			for uri in links:
+				URI = uri.attrs.get('href')
+				#print URI
+				if (('maps.google.com' not in URI) and ('www.bbb.org' not in URI)):
+					link = URI
+
+
+	#		bbb_parse_address(name, addr, phone, link, img, bbb_uri)
+	#		bbb_parse_business(bbb_uri)
+	#		bbb_parse_business_reviews(name, bbb_uri)
+
+
+	def update_company_directory(self):
+		tl_directories = self.bbb_top_directories()
+		for company_directory_by_type in tl_directories:
+			self.bbb_scrape_directory_page(company_directory_by_type)
+			#get_businesses_by_type(business_type)
+
+		for k, v in self.errors:
+			print 'Error on ', k, v
+
+	def get_company_directory(self):
+		full_directory = []
+		return full_directory
+
