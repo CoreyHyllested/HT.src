e313c7e9dcebde167e9356ac6fb43603cb19595c
Corey Hyllested <corey@herotime.co>
2015-06-30 12:48:14 -0600

    breakup __init__ into init groups

--- server/__init__.py
+++ server/__init__.py
@@ -1,13 +1,13 @@
 #################################################################################
 # Copyright (C) 2015 Soulcrafting
 # All Rights Reserved.
-# 
+#
 # All information contained is the property of Soulcrafting. Any intellectual
-# property about the design, implementation, processes, and interactions with 
-# services may be protected by U.S. and Foreign Patents. All intellectual 
-# property contained within is covered by trade secret and copyright law.   
-# 
-# Dissemination or reproduction is strictly forbidden unless prior written 
+# property about the design, implementation, processes, and interactions with
+# services may be protected by U.S. and Foreign Patents. All intellectual
+# property contained within is covered by trade secret and copyright law.
+#
+# Dissemination or reproduction is strictly forbidden unless prior written
 # consent has been obtained from Soulcrafting.
 #################################################################################
 
@@ -27,23 +27,38 @@ from server.infrastructure.srvc_database	import initialize_database
 from config import server_configuration
 
 
-
-log_frmtr = logging.Formatter('%(asctime)s %(levelname)s %(message)s')	#[in %(pathname)s:%(lineno)d]'))
-log_hndlr = StreamHandler() #logs to stderr? #RotatingFileHandler('/tmp/ht.log', 'a', 1024*1024, 10) 
-log_hndlr.setFormatter(log_frmtr)
-log_hndlr.setLevel(logging.INFO)
-
-ht_csrf  = CsrfProtect()
-sc_csrf	= ht_csrf
-ht_oauth = OAuth()
-sc_oauth = ht_oauth
-
-ht_server = None
 sc_server = None
 
-def create_upload_directory(sc_server):
+
+def initialize_server(config_name):
+	global sc_server
+	sc_server = Flask(__name__)
+
+	server_init_environment(sc_server, config_name)
+	server_init_logging(sc_server)
+
+	server_init_service_sessions(sc_server)
+	server_init_service_database(sc_server)
+	server_init_service_oauth(sc_server)
+
+	server_init_assets(sc_server)
+	server_init_routes(sc_server)
+
+	Compress(sc_server)
+	return sc_server
+
+
+
+def	server_init_environment(server, configuration):
+	server.secret_key = '\xfai\x17^\xc1\x84U\x13\x1c\xaeU\xb1\xd5d\xe8:\x08\xf91\x19w\x843\xee'
+	server.config.from_object(server_configuration[configuration])
+	if (configuration == 'production' or configuration == 'devel_money'):
+		print 'using configuration... ', configuration
+#	server.use_x_sendfile = server.config.get('USE_SENDFILE', False)	mod_xsendfile not installed on AWS/httpd
+
 	try:
-		dir_upload = sc_server.config['SC_UPLOAD_DIR']
+		# create upload directory.
+		dir_upload = server.config['SC_UPLOAD_DIR']
 		os.makedirs(dir_upload)
 	except OSError as oe:
 		if (oe.errno != 17):
@@ -54,31 +69,34 @@ def create_upload_directory(sc_server):
 		raise e
 
 
-def initialize_server(config_name):
-	global sc_server, ht_server
-	sc_server = Flask(__name__)
-	ht_server = sc_server # dont break everything yet
+def	server_init_logging(server):
+	log_frmtr = logging.Formatter('%(asctime)s %(levelname)s %(message)s')	#[in %(pathname)s:%(lineno)d]'))
+	log_hndlr = StreamHandler() #logs to stderr? #RotatingFileHandler('/tmp/ht.log', 'a', 1024*1024, 10) 
+	log_hndlr.setFormatter(log_frmtr)
+	log_hndlr.setLevel(logging.INFO)
 
-	sc_server.config.from_object(server_configuration[config_name])
-	if (config_name == 'production' or config_name == 'devel_money'):
-		print 'using configuration... ', config_name
-#	sc_server.use_x_sendfile = sc_server.config.get('USE_SENDFILE', False)	mod_xsendfile not installed on AWS/httpd
+	server.debug = True
+	server.logger.setLevel(logging.DEBUG)
+	server.logger.addHandler(log_hndlr)	 #sc_server.logger.addHandler(logging.FileHandler("/tmp/ht.log", mode="a"))
+	
+
+def server_init_service_sessions(server):
+	redis_cache = Redis(server)
+	server.session_interface = RedisSessionInterface(redis=redis_cache)
 
 
-	sc_server.secret_key = '\xfai\x17^\xc1\x84U\x13\x1c\xaeU\xb1\xd5d\xe8:\x08\xf91\x19w\x843\xee'
-	sc_server.debug = True
-	sc_server.logger.setLevel(logging.DEBUG)
-	sc_server.logger.addHandler(log_hndlr)	 #sc_server.logger.addHandler(logging.FileHandler("/tmp/ht.log", mode="a"))
-	create_upload_directory(sc_server)
+def server_init_service_database(server):
+	initialize_database(server.config)
 
-	redis_cache = Redis(sc_server)
-	sc_server.session_interface = RedisSessionInterface(redis=redis_cache)
-	initialize_database(sc_server.config)
 
-	sc_csrf.init_app(sc_server)
-	sc_oauth.init_app(sc_server)
+def server_init_service_oauth(server):
+	server.oauth = OAuth()
+	server.oauth.init_app(sc_server)
+	
 
-	jsfilter = sc_server.config['JSFILTER']
+def	server_init_assets(server):
+	jsfilter = server.config['JSFILTER']
+
 	# Note, Bundle looks for input files (e.g. 'js/format.js') and saves output files dir relative to '/static/'
 	js_dashboard_maps_format = Bundle('js/maps.js', 'js/format.js', filters=jsfilter, output='js/maps.format.js')
 	css_loginsys =	Bundle('scss/authorize.scss', filters='pyscss', output='css/authorize.css')
@@ -94,8 +112,8 @@ def initialize_server(config_name):
 	css_dashboard = Bundle('scss/dashboard.scss', filters='pyscss', output='css/pro_dashboard.css')
 	elem_header	=	Bundle('scss/elem-navigate.scss', 'scss/modals.scss', filters='pyscss', output='css/navigate.css')
 
-	assets = Environment(sc_server)
-	assets.url = sc_server.static_url_path
+	assets = Environment(server)
+	assets.url = server.static_url_path
 	assets.register('js_mapformat', js_dashboard_maps_format)
 	assets.register('scss_about_sc', css_about_sc)
 	assets.register('scss_errors',	css_errors)
@@ -110,14 +128,16 @@ def initialize_server(config_name):
 	assets.register('scss_dashboard', css_dashboard)
 	assets.register('navigate.css', elem_header)
 
-	Compress(sc_server)
+
+
+def server_init_routes(server):
+	server.csrf	= CsrfProtect()
+	server.csrf.init_app(server)
 
 	from routes import authentication, everyone, users, api, meta, errors, testing
 	from routes import sc_users as sc_allusers
 	from routes import sc_ebody as sc_everyone
 	from routes import sc_meta	as sc_metaserv
-	sc_server.register_blueprint(sc_everyone)
-	sc_server.register_blueprint(sc_allusers)
-	sc_server.register_blueprint(sc_metaserv)
-	return sc_server
-
+	server.register_blueprint(sc_everyone)
+	server.register_blueprint(sc_allusers)
+	server.register_blueprint(sc_metaserv)
