37f6d39c437afff43ddfad7b9e752aa69a52bbdc
Corey Hyllested <corey@herotime.co>
2015-06-30 10:05:12 -0600

    save helpful info (STARTMEUP / __init__ db cmds

--- server/models/__init__.py
+++ server/models/__init__.py
@@ -27,3 +27,56 @@ from .Availability		import *
 #from .GiftCertificate	import *
 #from .UserMessage		import *
 
+
+################################################################################
+#### EXAMPLE: Reading from PostgreSQL. #########################################
+################################################################################
+# psql "dbname=d673en78hg143l host=ec2-54-235-70-146.compute-1.amazonaws.com user=ezjlivdbtrqwgx password=lM5sTTQ8mMRM7CPM0JrSb50vDJ port=5432 sslmode=require"
+# psql postgresql://name:password@instance:port/database
+################################################################################
+
+
+################################################################################
+#### EXAMPLE: DELETING A ROW from PostgreSQL. ##################################
+################################################################################
+# delete from timeslot where location = 'San Francisco';
+# delete from timeslot where id >= 25;
+# delete from review where heroid = '559a73f1-483c-40fe-8ee5-83118ce1f7e3';
+################################################################################
+
+
+################################################################################
+#### EXAMPLE: ALTER TABLE from PostgreSQL. #####################################
+################################################################################
+# BEGIN;
+# LOCK appointments;
+# ALTER TABLE appointments ADD   COLUMN challenge   varchar(40);
+# ALTER TABLE appointments ALTER COLUMN transaction drop not null;
+# END;
+################################################################################
+# BEGIN;
+# UPDATE appointment2 SET buyer_prof = 62e9e608-12cd-4b47-9eb4-ff6998dca89a WHERE
+################################################################################
+
+
+################################################################################
+#### EXAMPLE: INSERT ROW INTO TABLE from PostgreSQL. ###########################
+################################################################################
+# begin;
+# insert into umsg (msg_id, msg_to, msg_from, msg_thread, msg_content, msg_created) VALUES ('testing-1-2-3-4-5', (select prof_id from profile where prof_name like '%Corey%'), (select prof_id from profile where prof_name like '%Frank%'), 'testing-1-2-3-4-5', 'Garbage in, garbage out', '2014-06-05 17:59:12.311562');
+# commit;
+################################################################################
+
+################################################################################
+#### EXAMPLE: FIND ALL DUPLICATE VALUES IN COLUMN. #############################
+################################################################################
+# begin;
+# select count(msg_thread), msg_thread from umsg group by msg_thread having count(msg_thread) > 1;
+# commit;
+#### RESULTS ###################################################################
+#  count |              msg_thread
+# -------+--------------------------------------
+#     3 | 1dfb5322-69c0-4a58-a768-ee66bcd1b9c6
+#     2 | 45eb702c-1e4c-4971-94dc-44b267930854
+#     5 | fd6d1310-809a-41bc-894a-da75b21aa315
+################################################################################
