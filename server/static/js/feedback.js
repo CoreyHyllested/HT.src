3e393898b0ba7f47ce19e51c63cb6ec1b1d6b6d3
Corey Hyllested <corey@herotime.co>
2015-08-06 15:30:12 -0600

    feedback.js standardizing show_errors()

--- server/static/js/feedback.js
+++ server/static/js/feedback.js
@@ -1,22 +1,37 @@
-var feedback_version = 0.10;
+var feedback_version = 0.13;
 
-function show_errors(status_element, errors) {
-	$(status_element).empty();
+function __get_json(string) {
+	try {
+		return JSON.parse(string);
+	} catch (e) {
+		console.log('String isnt (parsable) JSON');
+		console.log(string);
+		return null;
+	}
+}
 
-	$.each(errors, function(e, error){
+
+function show_errors(status_element, responseText) {
+	console.log("show_errors()");
+	set_status(status_element, 'There was an issue');
+
+	response = __get_json(responseText);
+	if (!response || !response.errors) return;
+
+	$.each(response.errors, function(e, error) {
 		// e is the element with the error, e.g. "prof_name"
 		var element = "#"+e;
 		console.log("show-error: ["+element + " : " + error + "]");
 //		$(element).prev(".ff.error").html(error).slideDown();
 		$(element).css("border-color", "#e75f63");
 	});
-	set_status(status_element, 'There was an issue');
 }
 function clear_error_msg(element) { $(element).prev(".ff.error").slideUp().html('');	}
 function clear_error_box(element) { $(element).css("border-color", "#e1e8ed");			}
 
 
 function set_status(query, content) {
+	$(query).empty();
 	$(query).html(content).fadeIn();
 	setTimeout(feedback_fade,	2500, query);
 }
