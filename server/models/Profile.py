b32b1fd31835119d9fad2be057a1fe7a788cacef
Corey Hyllested <corey@herotime.co>
2015-08-13 10:55:40 -0600

    replace Favicon w/ MysteryMan for /profile avatar

--- server/models/Profile.py
+++ server/models/Profile.py
@@ -144,7 +144,7 @@ class Profile(database.Model):
 
 
 	def gravatar_url(self):
-		default_icon = "https://soulcrafting.co/static/img/favicon.png"
+		default_icon = "mm" #no_pic.svg, https://soulcrafting.co/static/img/favicon.png
 		gravatar_url = "https://www.gravatar.com/avatar/" + hashlib.md5(self.prof_email.lower()).hexdigest() + "?"
 		return gravatar_url + urllib.urlencode({'d':default_icon, 's':str(85)})
 
